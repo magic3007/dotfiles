f3b16aae9d20f54189c116eecde28545e6938ac9
Jing Mai <jingmai@pku.edu.cn>
2025-03-06 16:35:56 +0800

    u

--- common_shell_setup.sh
+++ common_shell_setup.sh
@@ -172,3 +172,9 @@ alias lg='lazygit'
 
 # zellij
 alias zj="zellij"
+
+# flutter
+export PUB_HOSTED_URL=https://pub.flutter-io.cn
+export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+
+[ -f ~/.common_shell_setup_local.sh ] && source ~/.common_shell_setup_local.sh
