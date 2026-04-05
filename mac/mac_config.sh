344d38576661e68815915f55cfc44b03d6b2f1ef
Jing Mai <jingmai@pku.edu.cn>
2026-04-05 16:39:30 +0800

    feat(mac): Disable press-and-hold for specific applications
    Add configuration to disable the press-and-hold feature for various applications including VS Code, VS Code Insiders, and Windsurf. Also, remove the global setting for press-and-hold to ensure consistent behavior across the specified apps.

--- mac/mac_config.sh
+++ mac/mac_config.sh
@@ -4,3 +4,10 @@ defaults write NSGlobalDomain KeyRepeat -int 1 # Set blazingly fast key repeat r
 defaults write NSGlobalDomain InitialKeyRepeat 12  # Set blazingly fast initial key repeat rate.
 
 defaults write -g ApplePressAndHoldEnabled -bool false  # 关闭 Mac 长按字母键出现特殊字符（重音符号）菜单的功能
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
+defaults write com.vscodium ApplePressAndHoldEnabled -bool false                      # For VS Codium
+defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
+defaults write com.exafunction.windsurf ApplePressAndHoldEnabled -bool false          # For Windsurf
+defaults delete -g ApplePressAndHoldEnabled
